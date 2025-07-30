4bcafdf80d860382b86e1d2b7b7e1e4eab413f68
TurboMaKsIk <turbo.maksik12@gmail.com>
2025-07-30 23:14:17 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import './index.css'
+import tempIcon from './img/temperature.png'
+import humIcon from './img/humagince.png'
+import Sun from './img/sun.png'
 
 function App() {
   return (
-    <span></span>
+    <div className="container">
+      <header>
+        <h1 className='header-title'>WEATHER-APP</h1>
+        <form className='form-app' action="" method="post">
+          <input type="text" placeholder='введите город...'/>
+        </form>
+      </header>
+      <div className="main">
+        <div className="block-one">
+          <h1 className='block-temp'>32C*</h1>
+          <h1 className='block-time'>12:00</h1>
+          <div className="block-one-country">
+            <p>город</p>
+            <p>страна, регион</p>
+          </div>
+          <div className="block-one-data">
+            <div className="temp-data">
+              <span>Температура</span>
+              <p>12 C</p>
+            </div>
+            <div className="hum-data">
+              <span>Влажность</span>
+              <p>20 %</p>
+            </div>
+          </div>
+        </div>
+        <div className="block-two">
+          <img src={Sun} alt="" className='block-two-icon' />
+          <h1>Солнечно</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
