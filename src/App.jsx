837ace01749822f4ce81308a230c6fb5a6808ec6
TurboMaKsIk <turbo.maksik12@gmail.com>
2025-07-31 11:21:22 +0300

    добавление API

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
 import './index.css'
-import tempIcon from './img/temperature.png'
-import humIcon from './img/humagince.png'
-import Sun from './img/sun.png'
+import { useEffect, useState } from 'react';
+import Header from './component/Header';
+import Main from './component/Main';
+import React from 'react';
+
 
 function App() {
+  const [city, setCity] = useState('Serdobsk');
+  const [locations, setLocation] = useState({});
+  const [currents, setCurrent] = useState({condition: {text: 'none', icon: 'none'}});
+
+  useEffect(() => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=5affe47452424a17a68185535252407&q=${city}&aqi=no`)
+    .then(res => res.json())
+    .then(res => {
+      setLocation(res.location);
+      setCurrent(res.current);
+    })
+    .catch(err => console.log(err.message))
+  }, [city, setCity]);
+  console.log(currents);
   return (
     <div className="container">
-      <header>
-        <h1 className='header-title'>WEATHER-APP</h1>
-        <form className='form-app' action="" method="post">
-          <input type="text" placeholder='введите город...'/>
-        </form>
-      </header>
-      <div className="main">
-        <div className="block-one">
-          <h1 className='block-temp'>32C*</h1>
-          <h1 className='block-time'>12:00</h1>
-          <div className="block-one-country">
-            <p>город</p>
-            <p>страна, регион</p>
-          </div>
-          <div className="block-one-data">
-            <div className="temp-data">
-              <span>Температура</span>
-              <p>12 C</p>
-            </div>
-            <div className="hum-data">
-              <span>Влажность</span>
-              <p>20 %</p>
-            </div>
-          </div>
-        </div>
-        <div className="block-two">
-          <img src={Sun} alt="" className='block-two-icon' />
-          <h1>Солнечно</h1>
-        </div>
-      </div>
-      <p>© М. А. Шалаев, 2025</p>
+      <Header setCity={setCity}></Header>
+      <Main state={locations} currents={currents}/>
+      <p className='copyright'>© М. А. Шалаев, 2025</p>
     </div>
   );
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = ({ setCity }) => {
+
+    const submit = (e) => {
+        const post = e.get('city');
+        setCity(post);
+    }
+
+    return (
+        <header>
+            <h1 className='header-title'>WEATHER-APP</h1>
+            <form action={submit} className='form-app'>
+                <input name='city' type="text" placeholder='введите город...'/>
+            </form>
+        </header>
+    );
+}
+
+export default Header;
