9761b57b5094816aa74bc0e2397ab1d8f94cf277
TurboMaKsIk <turbo.maksik12@gmail.com>
2025-08-03 14:01:47 +0300

    forecast, css

--- src/component/ModuleForecastBox.jsx
+++ src/component/ModuleForecastBox.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import NewWeatherIcon from './NewWeatherIcon';
 
+const tootle = document.querySelectorAll('.imgTootle');
+
 
 const ModuleForecastBox = ({ locations }) => {
     return (
         <div className="forecast-day-box">
             <div className="forecast-day-time">{locations.time.slice(-5)}</div>
             <img src={locations.condition.icon} className="forecast-day-weather"/>
+            <div className='imgTootle'>{locations.condition.text}</div>
             <span></span>
             <div className="forecast-day-temp">{`${locations.temp_c}°`}</div>
         </div>
     );
 }
+
+
 
 export default ModuleForecastBox;
 
